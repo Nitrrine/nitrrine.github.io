95e6cf5b1d7d1fdbd4cd1926e6091041ae0054b0
Nitrrine <nitrrine@team.lodine.xyz>
2026-08-19 15:28:53 +0500

    chore: prefer single quote

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from "astro/config"
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nitrrine.github.io",
+  site: 'https://nitrrine.github.io',
   redirects: {
-    "/bluesky": "https://bsky.app/profile/nitrrine.lodine.xyz",
+    '/bluesky': 'https://bsky.app/profile/nitrrine.lodine.xyz',
   },
 })
